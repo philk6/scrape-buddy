0b4119b18f665a53fb43248ffd89c30a352a90d6
philk6 <philipkeipp@gmail.com>
2026-04-04 20:37:10 -0500

    Fix openfoodfacts.py: YELLOWWTHRESHOLD -> YELLOW_THRESHOLD

--- upc_providers/openfoodfacts.py
+++ upc_providers/openfoodfacts.py
@@ -12,7 +12,7 @@ from resolution_runtime import (
 from upc_base import UpcLookupProvider
 from upc_providers.scorer import (
     GREEN_THRESHOLD,
-    YELLOWWTHRESHOLD,
+    YELLOW_THRESHOLD,
     build_queries,
     score_candidate,
     score_to_confidence,
